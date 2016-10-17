b185337ad3409c67c0d0896a03946b3dcf239d73
Charles <crossan007@gmail.com>
2016-10-17 11:37:03 -0400

    Initial Commit

---
+++ API Enum.ps1
@@ -0,0 +1,78 @@
+﻿<#
+ .Synopsis
+ Enumerieren der vorhandenen Fenster
+#>
+
+<#
+ .Synopsis
+ Enumerieren der vorhandenen Fenster
+#>
+
+$TypeDef = @"
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Api
+{
+
+ public class WinStruct
+ {
+   public string WinTitle {get; set; }
+   public int WinHwnd { get; set; }
+ }
+
+ public class ApiDef
+ {
+   private delegate bool CallBackPtr(int hwnd, int lParam);
+   private static CallBackPtr callBackPtr = Callback;
+   private static List<WinStruct> _WinStructList = new List<WinStruct>();
+
+    [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+   [DllImport("User32.dll")]
+   [return: MarshalAs(UnmanagedType.Bool)]
+   private static extern bool EnumWindows(CallBackPtr lpEnumFunc, IntPtr lParam);
+
+    [DllImport("User32.dll")]
+   [return: MarshalAs(UnmanagedType.Bool)]
+   private static extern bool EnumChildWindows(int hWndParent, CallBackPtr lpEnumFunc, IntPtr lParam);
+
+   [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+   static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+   private static bool Callback(int hWnd, int lparam)
+   {
+       StringBuilder sb = new StringBuilder(256);
+       int res = GetWindowText((IntPtr)hWnd, sb, 256);
+      _WinStructList.Add(new WinStruct { WinHwnd = hWnd, WinTitle = sb.ToString() });
+       return true;
+   }   
+
+   public static List<WinStruct> GetWindows()
+   {
+      _WinStructList = new List<WinStruct>();
+      EnumWindows(callBackPtr, IntPtr.Zero);
+      return _WinStructList;
+   }
+
+   public static List<WinStruct> GetChildWindows(int parent)
+   {
+      _WinStructList = new List<WinStruct>();
+      EnumChildWindows(parent, callBackPtr, IntPtr.Zero);
+      return _WinStructList;
+   }
+
+ }
+}
+"@
+
+Add-Type -TypeDefinition $TypeDef -Language CSharpVersion3
+
+[Api.Apidef]::GetWindows() |
+ Where-Object { $_.WinTitle -like "*jab*" } 
+
---
+++ DisplaySetup.ps1
@@ -0,0 +1,84 @@
+﻿#https://msdn.microsoft.com/en-us/library/windows/desktop/ff468919(v=vs.85).aspx
+Add-Type -AssemblyName System.Windows.Forms
+Import-Module "$PSSCRIPTROOT\Set-Window.psm1" -force -verbose
+Add-Type @"
+    using System;
+    using System.Runtime.InteropServices;
+    public class Tricks {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+    }
+"@
+
+$Monitors = [system.windows.forms.screen]::AllScreens     
+
+If ($Monitors.count -ge 2)
+{
+    $tbHeight = 50
+
+    $mi=0
+    $p= Get-Process "FileMaker Pro" 
+    $x= $Monitors[$mi].bounds.x + $($Monitors[$mi].bounds.width/2)
+    $y= $Monitors[$mi].bounds.y
+    $h= $Monitors[$mi].bounds.height -$tbHeight
+    $w= $Monitors[$mi].bounds.width /2
+    $p.MainWindowHandle | Set-Window -X $x -y $y  -Height $h  -Width $w
+    [tricks]::SetForegroundWindow($p.MainWindowHandle)
+
+    $mi=0
+    $p= Get-Process "Outlook" 
+    $x= $Monitors[$mi].bounds.x
+    $y= $Monitors[$mi].bounds.y
+    $h= $Monitors[$mi].bounds.height -$tbHeight
+    $w= $Monitors[$mi].bounds.width /2
+    $p.MainWindowHandle | Set-Window -X $x -y $y  -Height $h  -Width $w
+    [tricks]::SetForegroundWindow($p.MainWindowHandle)
+
+    $mi=2
+    $p= Get-Process "Chrome" | ?{$_.MainWindowTitle} 
+    $x= $Monitors[$mi].bounds.x
+    $y= $Monitors[$mi].bounds.y
+    $h= $Monitors[$mi].bounds.height -$tbHeight
+    $w= $Monitors[$mi].bounds.width /2
+    $p.MainWindowHandle | Set-Window -X $x -y $y  -Height $h  -Width $w
+    [tricks]::SetForegroundWindow($p.MainWindowHandle)
+
+    $mi=2
+    $p= Get-Process "FireFox" | ?{$_.MainWindowTitle} 
+    $x= $($Monitors[$mi].bounds.x + [int]$($Monitors[$mi].bounds.width/2)) 
+    $y= $Monitors[$mi].bounds.y
+    $h=($($Monitors[$mi].bounds.height -$tbHeight )/2)
+    $w= $($Monitors[$mi].bounds.width/2)
+    $p.MainWindowHandle | Set-Window -X $x -y $y  -Height $h  -Width $w
+    [tricks]::SetForegroundWindow($p.MainWindowHandle)
+
+    $mi=2
+    $p= Get-Process "Iexplore" | ?{$_.MainWindowTitle} 
+    $x= $($Monitors[$mi].bounds.x + [int]$($Monitors[$mi].bounds.width/2)) 
+    $y= $Monitors[$mi].bounds.y + ($($Monitors[$mi].bounds.height)/2)
+    $h=($($Monitors[$mi].bounds.height -$tbHeight)/2)
+    $w= $($Monitors[$mi].bounds.width/2)
+    $p.MainWindowHandle | Set-Window -X $x -y $y  -Height $h  -Width $w
+    [tricks]::SetForegroundWindow($p.MainWindowHandle)
+
+     $mi=1
+    $p= Get-Process "RemoteDesktopManager" | ?{$_.MainWindowTitle} 
+    $x= $Monitors[$mi].bounds.x
+    $y= $Monitors[$mi].bounds.y 
+    $h= $Monitors[$mi].bounds.height -$tbHeight
+    $w= $($Monitors[$mi].bounds.width/2)
+    $p.MainWindowHandle | Set-Window -X $x -y $y  -Height $h  -Width $w
+    [tricks]::SetForegroundWindow($p.MainWindowHandle)
+
+     $mi=1
+    $p= Get-Process "SPDesign" | ?{$_.MainWindowTitle} 
+    $x= $($Monitors[$mi].bounds.x + $($Monitors[$mi].bounds.width/2) ) 
+    $y= $Monitors[$mi].bounds.y 
+    $h= $Monitors[$mi].bounds.height -$tbHeight
+    $w= $($Monitors[$mi].bounds.width/2)
+    $p.MainWindowHandle | Set-Window -X $x -y $y  -Height $h  -Width $w
+    [tricks]::SetForegroundWindow($p.MainWindowHandle)
+
+
+}
